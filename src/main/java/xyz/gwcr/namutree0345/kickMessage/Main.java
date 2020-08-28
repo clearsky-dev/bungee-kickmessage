808029443f9fd49f7353bc2b75afc04b328fc387
TreeKR <redwikitv@gmail.com>
2020-08-28 10:53:26 +0900

    First commit

---
+++ src/main/java/xyz/gwcr/namutree0345/kickMessage/Main.java
@@ -0,0 +1,10 @@
+package xyz.gwcr.namutree0345.kickMessage;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("KickMessage is enabled");
+    }
+}
