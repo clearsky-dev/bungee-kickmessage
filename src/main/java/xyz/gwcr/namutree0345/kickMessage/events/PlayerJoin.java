797fc074ad85ba79aefcfd197ca5e2e7da6cf973
TreeKR <redwikitv@gmail.com>
2020-08-28 13:18:39 +0900

    ok

--- src/main/java/xyz/gwcr/namutree0345/kickMessage/events/PlayerJoin.java
+++ src/main/java/xyz/gwcr/namutree0345/kickMessage/events/PlayerJoin.java
@@ -1,5 +1,6 @@
 package xyz.gwcr.namutree0345.kickMessage.events;
 
+import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -14,7 +15,6 @@ import java.util.HashMap;
 public class PlayerJoin implements Listener {
 
     ProxyServer server;
-    HashMap<ProxiedPlayer, Boolean> isCrashed = new HashMap<ProxiedPlayer, Boolean>();
 
     public PlayerJoin(ProxyServer server) {
         this.server = server;
@@ -22,26 +22,16 @@ public class PlayerJoin implements Listener {
 
     @EventHandler
     public void PostLogin(PostLoginEvent event) {
-        try {
-            event.getPlayer().connect(server.getServerInfo("lobby"));
-            isCrashed.put(event.getPlayer(), true);
-            System.out.println("[INFO] Connect to lobby");
-        } catch(Exception ex) {
-            System.out.println("[INFO] Caught error");
-            event.getPlayer().disconnect(new TextComponent("첫번째줄\n두번째줄"));
-        }
+            event.getPlayer().connect(server.getServerInfo("lobby"), new Callback<Boolean>() {
+                @Override
+                public void done(Boolean aBoolean, Throwable throwable) {
+                    if(throwable != null) {
+                        System.out.println("[INFO] Caught error");
+                        event.getPlayer().disconnect(new TextComponent("첫번째줄\n두번째줄"));
+                    }
+                }
+            });
         
-    }
-
-    public void Disconected(PlayerDisconnectEvent event) {
-
-    }
-
-    public void Connected(ServerConnectedEvent event) {
-        if(isCrashed.containsKey(event.getPlayer())) {
-            isCrashed.remove(event.getPlayer());
-            event.getPlayer().disconnect(new TextComponent("1째줄\n2째줄\n"));
-        }
     }
 
 }
